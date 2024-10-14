fc68ea15193abee45b8acc35d1d42e544c3915aa
陈墨 <1973657048@qq.com>
2024-10-15 00:19:23 +0800

    docs: 深拷贝

---
+++ cm-source/cloneObject.js
@@ -0,0 +1,14 @@
+const cloneObj = (obj) => {
+    var str, newObj = obj.constructor === Array ? [] : {};
+    if (typeof obj !== 'object') {
+        return;
+    } else if (window.JSON) {
+        str = JSON.stringify(obj), //系列化对象
+            newObj = JSON.parse(str); //还原
+    } else {
+        for (var i in obj) {
+            newObj[i] = typeof obj[i] === 'object' ? cloneObj(obj[i]) : obj[i];
+        }
+    }
+    return newObj;
+};
