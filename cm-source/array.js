bb6706c3dd31c21f8d92e015a1d6e27e290d08fc
陈墨 <1973657048@qq.com>
2024-10-16 00:09:21 +0800

    docs：数组去重

--- cm-source/array.js
+++ cm-source/array.js
@@ -7,3 +7,23 @@ function mixArr(arr) {
         return Math.random() - 0.5
     })
 }
+
+
+/**
+ * 数组去重
+ *  Array.from(new Set(arr))
+ *  [...new Set(arr)]
+ */
+
+function removeDup(arr) {
+    let result = []
+    let hashMap = {}
+    for (let i = 0; i < arr.length; i++) {
+        let temp = arr[i]
+        if (!hashMap[temp]) {
+            hashMap[temp] = true
+            result.push(temp)
+        }
+    }
+    return result
+}
